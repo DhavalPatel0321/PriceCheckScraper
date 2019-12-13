bd541ee9298760f9a73be35e24ae648bb71dc122
Dhaval Patel <dhavalp0321@gmail.com>
2019-12-13 14:58:50 -0500

    using dummy urls and user agents until i get the one he wants and maybe run this on a ec2 instance

--- scraper.py
+++ scraper.py
@@ -1,2 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+
+URL = ''
+
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
